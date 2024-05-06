15d255c453ead2a105e8985f6506b19c5982865a
jiaero <malns7@naver.com>
2024-05-07 00:59:07 +0900

    style: edit Header button onClick effect

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -95,6 +95,7 @@ export default function Header(props: HeaderProps) {
                 },
               }}
               size='small'
+              disableRipple
             >
               쇼핑
             </Button>
@@ -108,6 +109,7 @@ export default function Header(props: HeaderProps) {
                 },
               }}
               size='small'
+              disableRipple
             >
               커뮤니티
             </Button>
