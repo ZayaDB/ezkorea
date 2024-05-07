39a93bc27365c836b81599d16dcdcab1a9d13a1a
jiaero <malns7@naver.com>
2024-05-07 14:30:52 +0900

    style: Responsive Header UI

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+// import { Grid } from '@mui/material';
+import Carousel from 'react-bootstrap/Carousel';
+
+export default function Home() {
+  return (
+    <>
+      <div style={{ paddingTop: '120px' }}>Home</div>
+    </>
+  );
+}
