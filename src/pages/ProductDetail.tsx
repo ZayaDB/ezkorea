366c99393919d427ccc155ff7d620743450d182f
meenie49 <hjm904089@gmail.com>
2024-05-08 14:24:40 +0900

    style: purchasebar position fixed

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -9,20 +9,18 @@ import ProductCarousel from '../components/productDetail/ProductCarousel';
 export default function ProductDetail() {
   return (
     <div className='App'>
-      <ContentArea>
-        <div className='detailPage'>
-          <div className='detailMain'>
-            <ProductCarousel />
-            <div className='contents'>
-              <ProductTabs />
-              <SimilarProducts />
-            </div>
-          </div>
-          <div className='detailSide'>
-            <SelectPurchase />
+      <div className='detailPage'>
+        <div className='detailMain'>
+          <ProductCarousel />
+          <div className='contents'>
+            <ProductTabs />
+            <SimilarProducts />
           </div>
         </div>
-      </ContentArea>
+        <div className='detailSide'>
+          <SelectPurchase />
+        </div>
+      </div>
     </div>
   );
 }
