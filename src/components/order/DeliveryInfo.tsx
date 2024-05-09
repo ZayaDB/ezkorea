a9b73210e983fd830e0ff2ed7109d5469123cce3
JIN <jinorabel@gmail.com>
2024-05-10 01:50:05 +0900

    feat : 주소록 모달

--- src/components/order/DeliveryInfo.tsx
+++ src/components/order/DeliveryInfo.tsx
@@ -3,8 +3,9 @@ import { useSelector } from 'react-redux';
 
 import { useState } from 'react';
 import Head from './Head';
-import { FormControl, MenuItem, TextField } from '@mui/material';
+import { Box, FormControl, MenuItem, Modal, TextField } from '@mui/material';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
+import CloseButton from 'react-bootstrap/CloseButton';
 import Postcode from './Postcode';
 
 /* 수령자 정보 
@@ -39,6 +40,7 @@ export default function DeliveryInfo() {
         />
         새로운 배송지
       </label>
+      <AddressBook />
       <DeliveryInfoContent />
     </div>
   );
@@ -59,9 +61,9 @@ function DeliveryInfoContent() {
   return (
     <div className='deliveryinfo-container'>
       <TextField fullWidth placeholder='받으시는 분' />
-      <TextField placeholder='우편번호' value={zonecode} />
+      <TextField placeholder='우편번호' value={zonecode} disabled />
       <Postcode />
-      <TextField fullWidth placeholder='기본주소' value={address} />
+      <TextField fullWidth placeholder='기본주소' value={address} disabled />
       <TextField fullWidth placeholder='나머지주소' />
 
       <div className='orderer-num-container'>
@@ -85,5 +87,58 @@ function DeliveryInfoContent() {
       </div>
       <TextField fullWidth placeholder='배송 메시지' />
     </div>
+  );
+}
+
+/* 주소록 기능 */
+function AddressBook() {
+  /* 모달창 */
+  const style = {
+    position: 'absolute' as const,
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  /* 주소록 */
+  const [name] = useState<string>('진가영');
+  const [addressName] = useState<string>('집');
+  const [zonecode] = useState<string>('12345');
+  const [address] = useState<string>('서울특별시 어쩌구 저쩌구로 166');
+  const [phoneNum] = useState<string>('010-1111-2222');
+
+  return (
+    <>
+      <button onClick={handleOpen}>주소록</button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+      >
+        <Box sx={style}>
+          <CloseButton onClick={handleClose} />
+          <div className='address-caption'>배송지 목록</div>
+          <div>
+            <div>
+              {name}({addressName})
+            </div>
+            <div>
+              ({zonecode}) {address}
+            </div>
+            <div>{phoneNum}</div>
+          </div>
+        </Box>
+      </Modal>
+    </>
   );
 }
