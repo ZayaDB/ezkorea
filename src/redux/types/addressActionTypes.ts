3135e099a370d6923df92feffc8a2ae52a485182
JIN <jinorabel@gmail.com>
2024-05-10 14:00:48 +0900

    docs : toolkit으로 변경

--- src/redux/types/addressActionTypes.ts
+++ src/redux/types/addressActionTypes.ts
@@ -1,11 +1,17 @@
-export const UPDATE_ADDRESS_INFO = 'UPDATE_ADDRESS_INFO' as const;
+// export const UPDATE_ADDRESS_INFO = 'UPDATE_ADDRESS_INFO' as const;
 
-interface UpdateAddressInfoAction {
-  type: typeof UPDATE_ADDRESS_INFO;
-  payload: {
-    fullAddress: string;
-    zonecode: string;
-  };
+import { AddressState } from '../slices/addressSlice';
+
+// interface UpdateAddressInfoAction {
+//   type: typeof UPDATE_ADDRESS_INFO;
+//   payload: {
+//     fullAddress: string;
+//     zonecode: string;
+//   };
+// }
+
+// export type AddressActionTypes = UpdateAddressInfoAction;
+
+export interface AddressInfoSate {
+  address: AddressState;
 }
-
-export type AddressActionTypes = UpdateAddressInfoAction;
