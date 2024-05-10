d643476f1cd5132b22f46741ef044dcae405316a
meenie49 <hjm904089@gmail.com>
2024-05-10 12:08:43 +0900

    fix: pages/productDetail

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ContentArea from '../styles/ContentArea';
 import '../styles/productDetail/productDetail.scss';
 import SelectPurchase from '../components/productDetail/SelectPurchase';
 import ProductTabs from '../components/productDetail/ProductTabs';
