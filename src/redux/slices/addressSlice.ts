8edb624e3a09e88be1b09b40db05120c686bac9b
JIN <jinorabel@gmail.com>
2024-05-10 14:25:50 +0900

    docs : addressSlice 주석 지우기

--- src/redux/slices/addressSlice.ts
+++ src/redux/slices/addressSlice.ts
@@ -1,53 +1,6 @@
 // /* Actions */
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-// interface UpdateAddressInfoType {
-//   fullAddress: string;
-//   zonecode: string;
-// }
-
-// export const updateAddressInfo = (addressInfo: UpdateAddressInfoType) => ({
-//   type: UPDATE_ADDRESS_INFO,
-//   payload: {
-//     fullAddress: addressInfo.fullAddress,
-//     zonecode: addressInfo.zonecode,
-//   },
-// });
-
-// /* Reducer */
-// // 초기 상태의 타입 정의
-// interface AddressState {
-//   fullAddress: string;
-//   zonecode: string;
-// }
-
-// // 초기 상태
-// const initialState: AddressState = {
-//   fullAddress: '',
-//   zonecode: '',
-// };
-
-// type AddressActionType = ReturnType<typeof updateAddressInfo>;
-
-// // 리듀서 함수
-// const addressReducer = (
-//   state: AddressState = initialState, // 초기 상태의 타입으로 지정
-//   action: AddressActionType
-// ): AddressState => {
-//   switch (action.type) {
-//     case UPDATE_ADDRESS_INFO:
-//       return {
-//         ...state,
-//         fullAddress: action.payload.fullAddress,
-//         zonecode: action.payload.zonecode,
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
-// export default addressReducer;
-
 // initial state
 export interface AddressState {
   fullAddress: string;
