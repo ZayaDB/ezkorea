bfe21eac3d85efadc6352ccd08e034d79c48181d
meenie49 <hjm904089@gmail.com>
2024-05-12 05:02:51 +0900

    feat : pagination, addDiv, fetch

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,21 +1,51 @@
-import React from 'react';
-import '../styles/productDetail/productDetail.scss';
 import SelectPurchase from '../components/productDetail/SelectPurchase';
 import ProductTabs from '../components/productDetail/ProductTabs';
 import SimilarProducts from '../components/productDetail/SimilarProducts';
 import ProductCarousel from '../components/productDetail/ProductCarousel';
-import { Container } from '@mui/material';
+import { Container, useMediaQuery } from '@mui/material';
 
+// import { useNavigate, useParams } from 'react-router-dom';
+
+// const isMobile = useMediaQuery('(max-width: 768px)');
 export default function ProductDetail() {
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch('/data/prodReview.json');
+  //       const data = await response.json();
+  //       const reviews = data[0].reviews;
+  //       setReviewData(reviews);
+  //     } catch (error) {
+  //       console.error('Error fetching review data:', error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+  // const navigate = useNavigate();
+  // const params = useParams();
+
+  // const {productId} =useParams();
+
+  // const [result] = prodData.filter(
+  //   (prod) => prod.productId === Number(productId)
+  // );
+  const matches = useMediaQuery('(max-width:768px)');
   return (
     <div className='App'>
       <Container>
-        <div className='detailPage'>
+        <div className='detailPage' style={matches ? { width: '480px' } : {}}>
           <div className='detailMain'>
+            {/* navigation에 인자로 슬래시 없이 어떤 값을 보내면
+           현재 주소 뒤에 /작성한값 으로 이동 */}
+            {/* <button onClick={() => navigate('3')}></button> */}
+
             <ProductCarousel />
             <div className='contents'>
               <ProductTabs />
               <SimilarProducts />
+
+              {/* reviews props을 List 컴포넌트에 전달합니다. */}
             </div>
           </div>
           <div className='detailSide'>
--- src/types/productDetail.d.ts
+++ src/types/productDetail.d.ts
@@ -1,2 +1,51 @@
 declare module '*.jpg';
 declare module '*.PNG';
+
+export interface Review {
+  reviews: [];
+  reUserId: number;
+  reUserPhoto: string;
+  reUserName: string;
+  reDate: string;
+  rating: number;
+  contentPhoto: string;
+  contentText: string;
+}
+
+export interface Inquiry {
+  inquiry: [];
+  inUserId: number;
+  inUserName: string;
+  inDate: string;
+  inUserText: string;
+  inquiryType:
+    | 'product'
+    | 'refund'
+    | 'return'
+    | 'shipping'
+    | 'exchange'
+    | 'others';
+  answer: 'yes' | 'yet';
+  inBrDate: string;
+  inBrText: string;
+}
+
+export interface Product {
+  productId: string;
+  product_image: string;
+  brand_name: string;
+  product_name: string;
+  regular_price: string;
+  discount_rate: number;
+  discounted_price: string;
+  benefit: number;
+  commentCount: number;
+  colorOption: string[];
+  community_feed: string;
+  product_info_image: string;
+  reviews: Review[][];
+  delivery_refund: string;
+  inquiryTotal: number;
+  inquiry: Inquiry[];
+  related_products: string[];
+}
