318ed9b50e762df8c54a278e3611003eab0f97eb
JIN <jinorabel@gmail.com>
2024-05-13 00:00:01 +0900

    feat : 마일리지 유효성 검사

--- src/components/order/ApplyDiscount.tsx
+++ src/components/order/ApplyDiscount.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import Head from './Head';
 import { Button, TextField } from '@mui/material';
+
 /* 할인 적용
   - 쿠폰 : 쿠폰 적용 시 할인된 가격 표시
   - 포인트 : 1,000P 이상 사용 가능 멘트
@@ -10,6 +11,24 @@ export default function ApplyDiscount() {
 
   const [mileage] = useState<number>(1000);
   const [inputMileage, setInputMileage] = useState<number>();
+
+  const handleMileageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    // 입력값이 숫자인지 확인
+    const value = Number(event.target.value);
+    if (!isNaN(value)) {
+      // 숫자만 입력되도록 함
+      setInputMileage(value);
+    }
+  };
+
+  const applyAllMileage = () => {
+    setInputMileage(mileage);
+  };
+
+  // 마일리지 입력이 유효한지 검증하는 함수
+  const isValidMileage = (input: number | undefined) => {
+    return input !== undefined && input <= mileage;
+  };
 
   return (
     <div className='discount-container'>
@@ -23,6 +42,7 @@ export default function ApplyDiscount() {
       </div>
       <div className='discount-container-mileage-container'>
         <Head text='마일리지' />
+
         <div className='discount-container-mileage-container-content'>
           <div className='mileage-left'>
             <TextField
@@ -31,15 +51,16 @@ export default function ApplyDiscount() {
               placeholder='사용 금액 입력'
               value={inputMileage}
               size='small'
+              // 입력값이 변경될 때 유효성 검사 함수 호출
+              error={!isValidMileage(inputMileage)}
+              onChange={handleMileageChange}
             />
             <Button
               className='mileage-button'
               size='medium'
               variant='contained'
               color='secondary'
-              onClick={() => {
-                setInputMileage(mileage);
-              }}
+              onClick={applyAllMileage}
             >
               모두 사용
             </Button>
@@ -48,6 +69,11 @@ export default function ApplyDiscount() {
             <span className='lightgray'>보유 {mileage}P</span>
           </div>
         </div>
+        {!isValidMileage(inputMileage) && (
+          <span className='error-message red'>
+            입력된 마일리지가 보유한 마일리지를 초과합니다.
+          </span>
+        )}
       </div>
     </div>
   );
