39ec78f0450c96446f7dfc42f8c619d883be7f20
meenie49 <hjm904089@gmail.com>
2024-05-13 15:06:36 +0900

    feat: params설정

--- src/components/productDetail/ProductCarousel.tsx
+++ src/components/productDetail/ProductCarousel.tsx
@@ -4,18 +4,20 @@ import Carousel from 'react-bootstrap/Carousel';
 export default function ProductCarousel() {
   return (
     <>
-      <Carousel>
-        <Carousel.Item>
-          <div className='slidercontents1'></div>
+      <div>
+        <Carousel>
           {/* <img className='d-block w-100' /> */}
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className='slidercontents2'></div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className='slidercontents3'></div>
-        </Carousel.Item>
-      </Carousel>
+          <Carousel.Item>
+            <div className='slidercontents1'></div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className='slidercontents2'></div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className='slidercontents3'></div>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </>
   );
 }
