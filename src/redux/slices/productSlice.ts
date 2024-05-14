7afb87140447b85c832738a2a6adf609685b7cb6
JIN <jinorabel@gmail.com>
2024-05-15 00:17:04 +0900

    docs : product slice reducer 수정

--- src/redux/slices/productSlice.ts
+++ src/redux/slices/productSlice.ts
@@ -37,7 +37,7 @@ const productSlice = createSlice({
       state.selectedQuantity = action.payload;
     },
     setSelectedProductId: (state, action: PayloadAction<number>) => {
-      state.selectedQuantity = action.payload;
+      state.selectedProductId = action.payload;
     },
   },
 });
