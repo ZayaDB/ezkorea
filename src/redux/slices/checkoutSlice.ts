d2ff81c3779bbdfdeec1a1f92589ced14c8aa165
JIN <jinorabel@gmail.com>
2024-05-14 10:27:54 +0900

    feat : checkoutSlice OrdererInfo 추가

--- src/redux/slices/checkoutSlice.ts
+++ src/redux/slices/checkoutSlice.ts
@@ -1,30 +1,69 @@
 // /* Actions */
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import {
+  OrdererInfo,
+  OrderInfo,
+  DeliveryInfo,
+  PhoneNum,
+  Discount,
+  PaymentMethod,
+} from '../../types/checkoutTypes';
 
 // initial state
 export interface CheckoutState {
-  mileage: number;
+  ordererInfo: OrdererInfo[];
+  orderInfo: OrderInfo[];
+  deliveryInfo: DeliveryInfo[];
+  phoneNum: PhoneNum[];
+  discount: Discount[];
+  paymentMethod: PaymentMethod[];
 }
 
 /* 초기값 */
 const initialState: CheckoutState = {
-  mileage: 1000,
+  ordererInfo: [],
+  orderInfo: [],
+  deliveryInfo: [],
+  phoneNum: [],
+  discount: [],
+  paymentMethod: [],
 };
 
 // Create a slice
 export const CheckouteSlice = createSlice({
-  name: 'address',
+  name: 'checkout',
   initialState,
   reducers: {
-    updateMileage(state, action: PayloadAction<CheckoutState>) {
-      const { mileage } = action.payload;
-      state.mileage = mileage;
+    setOrdererInfo: (state, action: PayloadAction<OrdererInfo[]>) => {
+      state.ordererInfo = action.payload;
+    },
+    setOrderInfo: (state, action: PayloadAction<OrderInfo[]>) => {
+      state.orderInfo = action.payload;
+    },
+    setDeliveryInfo: (state, action: PayloadAction<DeliveryInfo[]>) => {
+      state.deliveryInfo = action.payload;
+    },
+    setPhoneNum: (state, action: PayloadAction<PhoneNum[]>) => {
+      state.phoneNum = action.payload;
+    },
+    setDiscount: (state, action: PayloadAction<Discount[]>) => {
+      state.discount = action.payload;
+    },
+    setPaymentMethod: (state, action: PayloadAction<PaymentMethod[]>) => {
+      state.paymentMethod = action.payload;
     },
   },
 });
 
 // Export the action creator
-export const { updateMileage } = CheckouteSlice.actions;
+export const {
+  setOrdererInfo,
+  setOrderInfo,
+  setDeliveryInfo,
+  setPhoneNum,
+  setDiscount,
+  setPaymentMethod,
+} = CheckouteSlice.actions;
 
 // Export the reducer
 export default CheckouteSlice.reducer;
