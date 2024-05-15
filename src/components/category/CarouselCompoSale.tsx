25cbaf930bbef7cae6ae9b46e96d90020be440b7
eun1230 <jsily2288@naver.com>
2024-05-15 17:15:48 +0900

    style: carousel수정

--- src/components/category/CarouselCompoSale.tsx
+++ src/components/category/CarouselCompoSale.tsx
@@ -1,5 +1,4 @@
 import Carousel from 'react-bootstrap/Carousel';
-import '../../styles/home/carousel.scss';
 import sale1 from '../../assets/images/sale1.webp';
 import sale2 from '../../assets/images/sale2.webp';
 import sale3 from '../../assets/images/sale3.webp';
@@ -13,17 +12,50 @@ export default function CarouselCompoSale() {
         두루루의 다양한 제품들을 특별한 가격에 만나보세요!
       </span>
       <div style={{ width: 'auto', margin: 0, padding: 0 }}>
-        <Carousel interval={5000}>
+        <Carousel className='carousel-container' interval={5000}>
           <Carousel.Item>
-            <img className='d-block w-100' src={sale1} alt='First slide' />
+            <img
+              className='d-block w-100'
+              src={sale1}
+              alt='First slide'
+              style={{
+                height: 'auto',
+                margin: '0 auto',
+                maxHeight: '450px',
+                objectFit: 'cover',
+                filter: 'brightness(60%) contrast(120%) grayscale(40%)',
+              }}
+            />
           </Carousel.Item>
 
           <Carousel.Item>
-            <img className='d-block w-100' src={sale2} alt='Second slide' />
+            <img
+              className='d-block w-100'
+              style={{
+                height: 'auto',
+                margin: '0 auto',
+                maxHeight: '450px',
+                objectFit: 'cover',
+                filter: 'brightness(60%) contrast(120%) grayscale(40%)',
+              }}
+              src={sale2}
+              alt='Second slide'
+            />
           </Carousel.Item>
 
           <Carousel.Item>
-            <img className='d-block w-100' src={sale3} alt='Third slide' />
+            <img
+              className='d-block w-100'
+              style={{
+                height: 'auto',
+                margin: '0 auto',
+                maxHeight: '450px',
+                objectFit: 'cover',
+                filter: 'brightness(60%) contrast(120%) grayscale(40%)',
+              }}
+              src={sale3}
+              alt='Third slide'
+            />
           </Carousel.Item>
         </Carousel>
       </div>
