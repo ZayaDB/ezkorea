9f943693bd8dbe3eba54b83d9782d8767123a3f1
eun1230 <jsily2288@naver.com>
2024-05-16 16:45:03 +0900

    feat: 통합검색페이지 및 비슷한 레이아웃페이지들 로딩처리

--- src/components/search/ResultProduct.tsx
+++ src/components/search/ResultProduct.tsx
@@ -14,9 +14,11 @@ const ResultProducts: React.FC<ResultProductsProps> = ({ keyword }) => {
 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true); // Set loading state to true when fetching new data
+      setLoading(true);
       try {
-        const { products: fetchedProducts } = await getData('/data/prodData.json');
+        const { products: fetchedProducts } = await getData(
+          '/data/prodData.json'
+        );
         const lowercaseKeyword = keyword.toLowerCase();
 
         const filtered = fetchedProducts.filter(
@@ -28,29 +30,36 @@ const ResultProducts: React.FC<ResultProductsProps> = ({ keyword }) => {
         );
 
         setFilteredProducts(filtered);
-        setLoading(false); // Set loading state to false after fetching and updating data
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching data:', error);
-        setLoading(false); // Ensure loading state is set to false in case of error
+        setLoading(false);
       }
     };
 
-    // Fetch data only if the keyword has changed
     if (keyword && keyword !== prevKeyword.current) {
       fetchData();
-      prevKeyword.current = keyword; // Update previous keyword with current keyword
+      prevKeyword.current = keyword;
     }
-  }, [keyword]); // Dependency array with keyword ensures useEffect runs when keyword changes
+  }, [keyword]);
 
   return (
     <div>
       {loading ? (
-        <div>Loading...</div>
+        <div style={{ textAlign: 'center' }}>Loading...</div>
       ) : (
         <div>
           {filteredProducts.length > 0 ? (
             <div className='likes-page'>
-              <div style={{ textAlign: 'center', fontSize: '13px', marginBottom: '10px' }}>상품결과</div>
+              <div
+                style={{
+                  textAlign: 'center',
+                  fontSize: '13px',
+                  marginBottom: '10px',
+                }}
+              >
+                
+              </div>
               <div className='likes-content'>
                 {filteredProducts.map((product: Products) => (
                   <ProductItem key={product.productId} prod={product} />
@@ -58,7 +67,11 @@ const ResultProducts: React.FC<ResultProductsProps> = ({ keyword }) => {
               </div>
             </div>
           ) : (
-            <div>상품 검색 결과가 없습니다.</div>
+            <div className='likes-content'>
+              <div style={{ textAlign: 'center' , width:'100%', marginTop:'100px' }}>
+                상품 검색 결과가 없습니다.
+              </div>
+            </div>
           )}
         </div>
       )}
