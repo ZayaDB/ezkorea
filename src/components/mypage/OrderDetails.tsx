d521ed7d409dcfde2dc77995498689cafa03afaa
JIN <jinorabel@gmail.com>
2024-05-16 21:09:19 +0900

    style : orderDetail css

--- src/components/mypage/OrderDetails.tsx
+++ src/components/mypage/OrderDetails.tsx
@@ -24,15 +24,13 @@ export default function OrderDetails() {
       <div className='order-container'>
         <div className='head'>주문배송조회</div>
         <div className='order-container-head'>
-          <div className='li1'>
-            상품정보
-            <Products />
-          </div>
+          <div className='li1'>상품정보</div>
           <div className='li2'>진행상태</div>
           <div className='li3'>배송비</div>
           <div className='li4'>구매확정 및 리뷰</div>
         </div>
       </div>
+      <Products />
     </div>
   );
 }
@@ -45,33 +43,82 @@ function Products() {
     (state: RootState) => state.product.selectedQuantity
   );
 
-  if (selectOption.length > 0) {
-    const Product = useSelector(
-      (state: RootState) => state.product.products[0]
-    );
-    console.log(Product);
-  }
+  const productImg = useSelector(
+    (state: RootState) => state.product.products[0].product_image
+  );
+
+  const brandName = useSelector(
+    (state: RootState) => state.product.products[0].brand_name
+  );
+  const productName = useSelector(
+    (state: RootState) => state.product.products[0].product_name
+  );
+  const productPrice = useSelector(
+    (state: RootState) => state.product.products[0].regular_price
+  );
+
+  const discountPrice = useSelector(
+    (state: RootState) => state.product.products[0].discounted_price
+  );
 
   return (
     <div className='product-container'>
-      <Card sx={{ maxWidth: '100%' }}>
-        <Box sx={{ display: 'flex' }}>
-          {/* <CardMedia
-            component='img'
-            alt='상품 이미지'
-            height='200'
-            image={Product.product_image}
-          />
-          <CardContent sx={{ width: '150%' }}>
-            <div>{Product.brand_name}</div>
-            <div>{Product.product_name}</div>
-            <div>옵션 : {selectOption}</div>
-            <div>
-              {addCommasToNumber(Product.regular_price)} 원/수량{' '}
-              {selectedQuantity}개
-            </div>
-          </CardContent> */}
-        </Box>
+      <Card
+        sx={{
+          maxWidth: '100%',
+
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        {selectOption.map((option, index) => (
+          <Box
+            sx={{
+              maxWidth: '100%',
+              display: 'flex',
+            }}
+            key={index}
+          >
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                width: '48%',
+                marginBottom: '10px',
+              }}
+              key={index}
+            >
+              <CardMedia
+                component='img'
+                alt='상품 이미지'
+                height='200'
+                image={productImg}
+              />
+              <CardContent sx={{ width: '60%' }}>
+                <div>{brandName}</div>
+                <div>{productName}</div>
+                <div>
+                  옵션 :
+                  <span id='option' key={index}>
+                    {' '}
+                    {option[index]}
+                  </span>
+                </div>
+                <div>
+                  <span className='regular-price'>
+                    {addCommasToNumber(productPrice)}
+                  </span>
+                  {'  '}
+                  {addCommasToNumber(discountPrice)} 원
+                  <div>수량 : {selectedQuantity[index]}개</div>
+                </div>
+              </CardContent>
+            </Box>
+            <Box sx={{ width: '20%' }}>결제완료</Box>
+            <Box sx={{ width: '20%' }}>무료배송</Box>
+            <Box sx={{ width: '5%' }}>-</Box>
+          </Box>
+        ))}
       </Card>
     </div>
   );
