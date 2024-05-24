8c3d9284bf7a0d68682309d2d7f040e63733444f
JIN <jinorabel@gmail.com>
2024-05-24 10:18:45 +0900

    feat : 주문번호 생성

--- src/hooks/shop/order/getOrderNumber.ts
+++ src/hooks/shop/order/getOrderNumber.ts
@@ -1,12 +1,13 @@
 import getTodayDate from './getTodayDate';
 
-export default function getOrderNumber(): string {
+export default function getOrderNumber(): [string, string] {
   const datePart = getTodayDate('YYMMDD');
 
-  // 랜덤한 3자리 숫자 생성 (시간 기반)
+  // 랜덤한 3자리 숫자 생성 (100에서 999 사이)
   const randomPart: string = Math.floor(100 + Math.random() * 900).toString();
 
-  // 주문번호 생성하여 반환
-  const orderNumber: string = datePart + randomPart;
+  // 주문번호 생성하여 배열로 반환
+  const orderNumber: [string, string] = [`${datePart}-${randomPart}`, datePart];
+
   return orderNumber;
 }
