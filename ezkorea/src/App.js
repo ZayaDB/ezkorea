2b0fc7d592ee3a33a51144bbe2a62b1ec2179b8e
zaya <zayadreambig@gmail.com>
2025-01-27 18:40:04 +0900

    db to connect

--- ezkorea/src/App.js
+++ ezkorea/src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
-
+import Signup from "../src/pages/Signup";
+import Login from "../src/pages/Login";
+import Home from "../src/pages/Home";
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="register" element={<Signup></Signup>}></Route>
+          <Route path="login" element={<Login></Login>}></Route>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="header" element={<Header></Header>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
-
 export default App;
---
+++ ezkorea/src/components/Header.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const Header = () => {
+  return <></>;
+};
+export default Header;
