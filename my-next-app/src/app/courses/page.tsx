5e8a19236080c69b126a67eb0c0c76ccc0a5959d
envygod-dev <mbsavage0930@gmail.com>
2025-03-17 04:43:42 +0000

    run build-хийхэд хоосон файл оруулж болохгүй

---
+++ my-next-app/src/app/courses/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CoursesPage = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>Welcome to the Courses page.</p>
+    </div>
+  );
+};
+
+export default CoursesPage;
+
